f3a4c52945040a508ce7ab4d857db2b1996499be
hcbb8 <hcbb8@126.com>
2023-02-13 16:39:18 +0800

    Update kwyy.js

--- rules/kwyy.js
+++ rules/kwyy.js
@@ -1,6 +1,6 @@
 *******************************
 [rewrite_local]
-^https?:\/\/.*\.(kuwo|lrts)\.(cn|me)\/(a\.p|music\.pay|(vip\/(v2|enc)\/(theme|user\/vip))|(EcomResource|(Mobile)?Ad)Serv(er|ice)).* url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/kwyy.js
+^https?:\/\/.*\.(kuwo|lrts)\.(cn|me)\/(a\.p|music\.pay|(vip\/(v2|enc)\/(theme|user\/vip))|(EcomResource|(Mobile)?Ad)Serv(er|ice)).* url script-response-body https://raw.githubusercontent.com/hcbb8/snail/main/rules/kwyy.js
 [mitm] 
 hostname = *.kuwo.cn, *.lrts.me
 ********************************/
